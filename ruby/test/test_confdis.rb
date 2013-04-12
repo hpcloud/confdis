8eefa08fa3813d647cb281b2d99add50f017de65
Sridhar Ratnakumar <sridharr@activestate.com>
2013-04-12 13:29:09 -0700

    initial ruby implementation
    minus watcher (not working yet)

--- ruby/test/test_confdis.rb
+++ ruby/test/test_confdis.rb
@@ -1,7 +1,14 @@
 require 'helper'
 
 class TestConfdis < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "connect properly" do
+    # TODO: test watcher
+    name = "Primates - #{Time.now}"
+    c = ConfDis.new "localhost", 6379, "test:confdis:simple"
+    c2 = ConfDis.new "localhost", 6379, "test:confdis:simple"
+    c.data[:name] = name
+    c.save
+    sleep 1
+    # assert_equal name, c2.data[:name]
   end
 end
