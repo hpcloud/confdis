ace919d90ffa83ed758fef739f6bd2761f28ff23
Sridhar Ratnakumar <sridharr@activestate.com>
2013-04-23 14:29:29 -0700

    prevent mutation of old struct values
    upon every change, load the new config in a new struct value, forcing
    users to re-read from the new struct (better API upcoming)

--- go/confdis/confdis.go
+++ go/confdis/confdis.go
@@ -5,32 +5,46 @@ import (
 	"encoding/json"
 	"github.com/vmihailenco/redis"
 	"net"
+	"reflect"
 )
 
+type structCreator func() interface{}
+
 type ConfDis struct {
-	rootKey      string
-	pubChannel   string
-	configStruct interface{}
-	redis        *redis.Client
-	Changes      chan error // Channel to receive config updates (value is return of reload())
+	rootKey    string
+	structType reflect.Type
+	pubChannel string
+	Config     interface{}
+	redis      *redis.Client
+	Changes    chan error // Channel to receive config updates (value is return of reload())
 }
 
-func New(addr, rootKey string, struc interface{}) (*ConfDis, error) {
-	c := ConfDis{rootKey, rootKey + ":_changes", struc, nil, make(chan error)}
+func New(addr, rootKey string, structVal interface{}) (*ConfDis, error) {
+	c := ConfDis{
+		rootKey,
+		reflect.TypeOf(structVal),
+		rootKey + ":_changes",
+		createStruct(reflect.TypeOf(structVal)),
+		nil,
+		make(chan error)}
 	if err := c.connect(addr); err != nil {
 		return nil, err
 	}
-	if err := c.reload(); err != nil {
+	if _, err := c.reload(); err != nil {
 		return nil, err
 	}
 	return &c, c.watch()
+}
+
+func createStruct(t reflect.Type) interface{} {
+	return reflect.New(t).Interface()
 }
 
 // Save saves current config onto redis. WARNING: Save() may not work
 // correctly if there are concurrent changes from other clients
 // (notified via pubsub).
 func (c *ConfDis) Save() error {
-	if data, err := json.Marshal(c.configStruct); err != nil {
+	if data, err := json.Marshal(c.Config); err != nil {
 		return err
 	} else {
 		if r := c.redis.Set(c.rootKey, string(data)); r.Err() != nil {
@@ -57,12 +71,22 @@ func (c *ConfDis) connect(addr string) error {
 }
 
 // reload reloads the config tree from redis.
-func (c *ConfDis) reload() error {
-	// FIXME: must zero-value c.configStruct before overwriting it.
+func (c *ConfDis) reload() (interface{}, error) {
+	// FIXME: must zero-value c.Config before overwriting it.
+	// TODO: and when doing so, keep the old config value for sending
+	// in Changes channel (cf. drain change notifications).
 	if r := c.redis.Get(c.rootKey); r.Err() != nil {
-		return r.Err()
+		return nil, r.Err()
 	} else {
-		return json.Unmarshal([]byte(r.Val()), c.configStruct)
+		config2 := createStruct(c.structType)
+		if err := json.Unmarshal([]byte(r.Val()), config2); err != nil {
+			return nil, err
+		}
+		// FIXME: make it atomic in conjunction with the edits prior
+		// to Save()
+		config1 := c.Config
+		c.Config = config2
+		return config1, nil
 	}
 	panic("unreachable")
 }
@@ -83,7 +107,9 @@ func (c *ConfDis) watch() error {
 	go func() {
 		for {
 			<-ch
-			c.Changes <- c.reload()
+			_, err := c.reload()
+			// TODO: pass old config (_) if necessary in the future.
+			c.Changes <- err
 		}
 	}()
 
--- go/confdis/confdis_test.go
+++ go/confdis/confdis_test.go
@@ -15,14 +15,11 @@ type SampleConfig struct {
 }
 
 func TestSimple(t *testing.T) {
-	var config SampleConfig
-	c, err := New(
-		"localhost:6379",
-		"test:confdis:simple",
-		&config)
+	c, err := New("localhost:6379", "test:confdis:simple", SampleConfig{})
 	if err != nil {
 		t.Fatal(err)
 	}
+	config := c.Config.(*SampleConfig)
 	config.Name = "primates"
 	config.Users = []string{"chimp", "bonobo", "lemur"}
 	config.Meta.Researcher = "Jane Goodall"
@@ -32,11 +29,10 @@ func TestSimple(t *testing.T) {
 
 func TestChangeNotification(t *testing.T) {
 	// Seed data, using the first client
-	var config SampleConfig
 	c, err := New(
 		"localhost:6379",
 		"test:confdis:notify",
-		&config)
+		SampleConfig{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -47,6 +43,7 @@ func TestChangeNotification(t *testing.T) {
 			}
 		}
 	}()
+	config := c.Config.(*SampleConfig)
 	config.Name = "primates-changes"
 	config.Users = []string{"chimp", "bonobo", "lemur"}
 	config.Meta.Researcher = "Jane Goodall"
@@ -56,33 +53,35 @@ func TestChangeNotification(t *testing.T) {
 	time.Sleep(time.Duration(100 * time.Millisecond))
 
 	// Second client
-	var config2 SampleConfig
-	if c2, err := New(
+	c2, err := New(
 		"localhost:6379",
 		"test:confdis:notify",
-		&config2); err != nil {
+		SampleConfig{})
+	if err != nil {
 		t.Fatal(err)
-	}else{
-		go func() {
-			for err := range c2.Changes {
-				if err != nil {
-					t.Fatal(err)
-				}
-			}
-		}()
 	}
+	go func() {
+		for err := range c2.Changes {
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	}()
+	config2 := c2.Config.(*SampleConfig)
 
 	if config2.Meta.Researcher != "Jane Goodall" {
 		t.Fatal("different value")
 	}
 
 	// Trigger a change via the first client
+	config = c.Config.(*SampleConfig)
 	config.Meta.Researcher = "Francine Patterson"
 	c.Save()
 	// Allow reasonable delay for network/redis latency
 	time.Sleep(time.Duration(100 * time.Millisecond))
 
 	// Second client must get notified of that change
+	config2 = c2.Config.(*SampleConfig)
 	if config2.Meta.Researcher != "Francine Patterson" {
 		t.Fatal("did not receive change")
 	}
