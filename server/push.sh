b3a30fb4307a5f35f504b185c0dca34f344171e8
Stefan Bourlon <stefanb@activestate.com>
2015-07-16 12:13:04 -0700

    302141 - server/push.sh - Log in the docker registry automatically

--- server/push.sh
+++ server/push.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 set -e # Exit on errors
 
-# Tag your local image to DOCKER_REGISTERY/image and push it
-: ${EMAIL?"Need to set EMAIL (e.g. export EMAIL=username@activestate.com)"}
-: ${REGISTRY_USERNAME?"Need to set REGISTRY_USERNAME (e.g. export REGISTRY_USERNAME=your_username)"}
-: ${REGISTRY_PASSWORD?"Need to set REGISTRY_PASSWORD (e.g. export REGISTRY_PASSWORD=your_password)"}
-
 image="$1"           # Name of the image to push
 docker_registry="$2" # Address of the docker registry
 
-# Check if a docker registry is already in the image name
-if echo ${image} | grep --quiet ${docker_registry}; then
-        >&2 echo "Docker registry ${docker_registry} found in the image $image"
-        exit 1
+# Log in the docker registry
+if ! echo | docker login $docker_registry; then
+        : ${EMAIL?"Need to set EMAIL (e.g. export EMAIL=username@activestate.com)"}
+        : ${REGISTRY_USERNAME?"Need to set REGISTRY_USERNAME (e.g. export REGISTRY_USERNAME=your_username)"}
+        : ${REGISTRY_PASSWORD?"Need to set REGISTRY_PASSWORD (e.g. export REGISTRY_PASSWORD=your_password)"}
+
+        docker login --email="$EMAIL" --username="$REGISTRY_USERNAME" --password="$REGISTRY_PASSWORD" $docker_registry
 fi
 
-docker tag ${image} ${docker_registry}/${image}
-docker login --email="$EMAIL" --username="$REGISTRY_USERNAME" --password="$REGISTRY_PASSWORD" $docker_registry
+# Tag the image with the docker registry if not already done
+if ! echo ${image} | grep --quiet ${docker_registry}; then
+        docker tag ${image} ${docker_registry}/${image}
+fi
+
+# Push the image to the docker registry
 docker push ${docker_registry}/${image}
 
